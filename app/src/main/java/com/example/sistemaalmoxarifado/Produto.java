4bb41bfc3ce8133c746dd75c02d140808aaa2874
italomarco <italogec@gmail.com>
2021-09-10 08:24:54 -0300

    TelaCadastro

---
+++ app/src/main/java/com/example/sistemaalmoxarifado/Produto.java
@@ -0,0 +1,10 @@
+package com.example.sistemaalmoxarifado;
+
+public class Produto {
+
+    private Integer id;
+    private String nome_produto;
+    private int codigo_produto;
+    private int quantidade_produto;
+
+}
